7b860038ce2646cb99d09a4f15853df8065a1f0f
Rosariogorosito <101011246+Rosariogorosito@users.noreply.github.com>
2024-03-05 10:40:20 -0300

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/su_nombre_de_usuario/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('su_nombre_de_usuario')
-            node / gitConfigEmail('el_mail_de_su_cuenta_de_GitHub')
+        git('https://github.com/Rosariogorosito/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('Rosariogorosito')
+            node / gitConfigEmail('aldanagorosito802@gmail.com')
         }
     }
  
@@ -15,7 +15,7 @@ job('Aplicacion Node.js DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('su_nombre_de_usuario/nodejsapp')
+            repositoryName('Rosariogorosito/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('El ID con el que hayan guardado la credencial de su cuenta de docker en jenkins')
             forcePull(false)
