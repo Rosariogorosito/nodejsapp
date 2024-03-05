ba67068988020a455dd9225dca46d06818081ff7
Rosariogorosito <101011246+Rosariogorosito@users.noreply.github.com>
2024-03-05 16:00:20 -0300

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -16,7 +16,7 @@ job('Aplicacion Node.js Docker DSL') {
     steps {
         dockerBuildAndPublish {
             repositoryName('Rosariogorosito/nodejsapp')
-            tag('${GIT_REVISION,length=7}')
+            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}')
             registryCredentials('Docker-hub')
             forcePull(false)
             createFingerprints(false)
